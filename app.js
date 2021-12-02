e91a5d2053be56e488e3a02b12c54da3d800e859
devyosse <narutoshippuden884@gmail.com>
2021-12-02 16:52:38 +0100

    chi-fu-mi

---
+++ app.js
@@ -0,0 +1,34 @@
+const rock = document.getElementById('pierre');
+const paper = document.getElementById('feuille');
+const scissors = document.getElementById('ciseau');
+const resultat = document.getElementById('result');
+let play = x = 0 , y = 0;
+const count = document.getElementById('score');
+const ordi = [
+    rock ,paper ,scissors
+];
+
+function jeu(element, egalite, loose, win){
+    element.addEventListener('click', function (){
+        let rdm = Math.floor(Math.random()*ordi.length);
+        play++;
+        if ( ordi[rdm] === egalite){
+            resultat.innerHTML = "Egalité";
+            ;
+        }
+        if(ordi[rdm] === loose){
+            resultat.innerHTML = "Perdu";
+            y++;
+        }
+        if(ordi[rdm] === win){
+            resultat.innerHTML = "Gagné";
+            x++;
+        }
+        count.innerHTML = "Victoire = " + x + "  Défaite = " + y;
+    })
+
+}
+
+jeu(rock, rock, paper, scissors);
+jeu(paper, paper, scissors, rock);
+jeu(scissors, scissors, rock, paper);
